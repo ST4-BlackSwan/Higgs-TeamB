4c23657a1c4c24c35af94f0d57c0e167a5c51f5c
CedrakNedrak <cedric.vilandrau@gmail.com>
2026-06-02 14:34:24 +0200

    Black reformatage

--- BDT/main.py
+++ BDT/main.py
@@ -1,9 +1,7 @@
 from HiggsML.datasets import download_dataset
 from boosted_decision_tree import BoostedDecisionTree
 
-data = download_dataset(
-    "blackSwan_data"
-)
+data = download_dataset("blackSwan_data")
 
 data.load_train_set()
 data_set = data.get_train_set()
