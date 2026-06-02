b5f46d98a8904b0acdf817e252d16dd205a49156
CedrakNedrak <cedric.vilandrau@gmail.com>
2026-06-02 10:27:34 +0200

    First try on tes

--- Syst/FirstTryLES.py
+++ Syst/FirstTryLES.py
@@ -1,5 +1,17 @@
 from HiggsML.systematics import systematics
-from HiggsML.datasets import download_dataset
+from HiggsML.datasets import download_dataset*
+
+"""Index(['PRI_lep_pt', 'PRI_lep_eta', 'PRI_lep_phi', 'PRI_had_pt', 'PRI_had_eta',
+       'PRI_had_phi', 'PRI_jet_leading_pt', 'PRI_jet_leading_eta',
+       'PRI_jet_leading_phi', 'PRI_jet_subleading_pt',
+       'PRI_jet_subleading_eta', 'PRI_jet_subleading_phi', 'PRI_n_jets',
+       'PRI_jet_all_pt', 'PRI_met', 'PRI_met_phi', 'weights',
+       'detailed_labels', 'labels', 'DER_mass_transverse_met_lep',
+       'DER_mass_vis', 'DER_pt_h', 'DER_deltaeta_jet_jet', 'DER_mass_jet_jet',
+       'DER_prodeta_jet_jet', 'DER_deltar_had_lep', 'DER_pt_tot', 'DER_sum_pt',
+       'DER_pt_ratio_lep_had', 'DER_met_phi_centrality',
+       'DER_lep_eta_centrality'],
+      dtype='str')"""
 
 # Charger les données normales
 data_normal = download_dataset(
