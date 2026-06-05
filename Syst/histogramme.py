c350f2440dc32a633869e639c812cc91f76ea076
Gapro Sammo <noemail>
2026-06-05 11:33:21 +0200

    Correction

--- Syst/histogramme.py
+++ Syst/histogramme.py
@@ -8,17 +8,20 @@ import matplotlib.pyplot as plt
 def histogramme(par, parnormal, listevalpar, intervalscoretotal, number_of_bins):
     delta_s, delta_b=[], []
     intervallen = intervalscoretotal[1] - intervalscoretotal[0]
-    for bin in range(number_of_bins):
+    """  for bin in range(number_of_bins):
         liste_delta_s, liste_delta_b = varpar(par, parnormal, listevalpar,np.linspace(intervalscoretotal+intervallen*bin/number_of_bins, intervalscoretotal+intervallen*(bin+1)/number_of_bins,10))
         delta_s.append(liste_delta_s)
         delta_b.append(liste_delta_b)
+    """
+    bin = np.linspace(0, number_of_bins, number_of_bins)
+    liste_delta_s, liste_delta_b = varpar(par, parnormal, listevalpar,np.linspace(intervalscoretotal+intervallen*bin/number_of_bins, intervalscoretotal+intervallen*(bin+1)/number_of_bins,10))
     delta_s = np.transpose(delta_s)
     delta_b = np.transpose(delta_b)
     for p in range(len(delta_s)):
         plt.plot(np.linspace(intervalscoretotal[0], intervalscoretotal[1], number_of_bins), delta_s[p])
         plt.plot(np.linspace(intervalscoretotal[0], intervalscoretotal[1], number_of_bins), delta_b[p])
 
-histogramme('tes', 1, np.linspace(0.97, 1.03, 10, [0.7, 1.0], 10))
-histogramme('jes', 1, np.linspace(0.97, 1.03, 10, [0.7, 1.0], 10))
-histogramme('bkg_norm', 1, np.linspace(0.95, 1.05, 10, [0.7, 1.0], 10))
-histogramme('soft_met', 1, np.linspace(0, 3, 10, [0.7, 1.0], 10))
+histogramme('tes', 1, np.linspace(0.97, 1.03, 10), [0.7, 1.0], 10)
+histogramme('jes', 1, np.linspace(0.97, 1.03, 10), [0.7, 1.0], 10)
+histogramme('bkg_norm', 1, np.linspace(0.95, 1.05, 10), [0.7, 1.0], 10)
+histogramme('soft_met', 1, np.linspace(0, 3, 10), [0.7, 1.0], 10)
